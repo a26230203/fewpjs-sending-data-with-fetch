f8aaa90defa256a56aba56294cde18c5bd892fc4
Bin <binlin35@yahoo.com>
2021-04-11 21:35:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name : `${name}`,
+            email : `${email}`
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => {
+        return response.json();
+    })
+    .then(date => {
+        console.log(date)
+    })
+    .catch((error) => {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    })
+}
+

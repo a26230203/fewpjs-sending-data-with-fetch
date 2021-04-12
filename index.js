7b6bd721d143ba91f65e7a9d355ef1411612d29e
Bin <binlin35@yahoo.com>
2021-04-11 21:32:27 -0400

    commit tree 25d84d41e8f2f284bb746b77dae06e8aefd3d3eb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name : `${name}`,
+            email : `${email}`
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => {
+         response.json();
+    })
+    .then(obj => {
+        console.log(obj)
+    })
+    .catch((error) => {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    })
+}
+
